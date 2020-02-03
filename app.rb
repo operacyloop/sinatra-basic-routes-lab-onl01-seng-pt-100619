35a2fc844ad1de6796e1112b00a08b05809426d1
Barry Ivan Silber <age.ef.ess@gmail.com>
2020-02-03 16:31:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-get 'name/' do
+get '/name' do
   "My name is __"
 end
 
-get 'hometown/' do
+get '/hometown' do
   "My hometown is __"
 end
 
-get 'favorite-song/' do
+get '/favorite-song' do
   "My favorite song is __"
 end
 
